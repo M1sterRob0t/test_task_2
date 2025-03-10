b5733053728072570778878257ac61552f92d294
M1ster Rob0t <baks750@mail.ru>
2025-03-10 08:38:06 +0300

    extracted Row component

--- src/componets/Table/index.tsx
+++ src/componets/Table/index.tsx
@@ -1,43 +1,9 @@
 import classNames from 'classnames';
 
-import { Sort, SortType, Status, Test } from '../../types';
-import { AppRoute } from '../../constants';
-import { NavButton } from '../UI/NavButton';
+import { Sort, SortType, Test } from '../../types';
 
 import './Table.scss';
-import { Colors, TestStatusToModifierDict } from './constants';
-
-const colors = Object.values(Colors);
-
-interface RowProps {
-  test: Test;
-  className?: string;
-}
-
-export const Row = ({ className, test }: RowProps) => {
-  const { id, name, site, status, type } = test;
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-  return (
-    <div className={classNames('row', className, `row__${randomColor}`)}>
-      <div className="row__cell row__name">{name}</div>
-      <div className="row__cell row__type">{type}</div>
-      <div className={`row__cell row__status row__status--${TestStatusToModifierDict[status]}`}>
-        {status}
-      </div>
-      <div className="row__cell row__site">{site}</div>
-      <div className="row__cell row__nav">
-        {status === Status.DRAFT ? (
-          <NavButton to={`${AppRoute.Finalize}/${id}`} isDark>
-            Finilize
-          </NavButton>
-        ) : (
-          <NavButton to={`${AppRoute.Results}/${id}`}>Results</NavButton>
-        )}
-      </div>
-    </div>
-  );
-};
+import { Row } from './components/Row';
 
 interface TableProps {
   tests: Test[];
